6d71ba8bbb9a16d8f5a22d9075a6532b91fec8a2
Ezra Obiwale <contact@ezraobiwale.com>
2017-08-24 20:12:38 +0100

    Ready for use

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # bitlyze
 Shorten long urls with Bitly
+
+## Usage
+
+```html
+<ul>
+    <li><a href="#" data-url="http://example.com/link1">Link 1</a></li>
+    <li><a href="#" data-url="http://example.com/link2">Link 3</a></li>
+    <li><a href="#" data-url="http://example.com/link3">Link 3</a></li>
+</ul>
+<!-- include js -->
+<script src="/path/to/jquery.min.js"></script>
+<!-- include bitlyze -->
+<script src="bitlyze.min.js"></script>
+<script>
+    $(function(){
+        $('a').bitlyze({
+            accessToken: 'bitly-access-token-here'
+        });
+    });
+</script>
+```
+
+The full config object is:
+
+```js
+{`
+    bitlyUrl: 'https://api-ssl.bitly.com/v3/shorten',``
+    accessToken: '',
+    urlDataKey: 'url',
+    callback: function (short_url) { }
+}
+```
+
+The callback is called on each of the elements, therefore the `context` (`this`)
+is the element.
+
+## License
+
+MIT``
---
+++ bitlyze.js
@@ -0,0 +1,61 @@
+(function ($) {
+    $.fn.bitlyze = function (settings) {
+        var btz = {}, str = localStorage.getItem('btz');
+        if (str) btz = JSON.parse(str);
+
+        var config = {
+            bitlyUrl: 'https://api-ssl.bitly.com/v3/shorten',
+            accessToken: '',
+            urlDataKey: 'url',
+            callback: function (short_url) { }
+        };
+        $.extend(config, settings);
+        this.each(function () {
+            var $link = $(this),
+                url = $link.data(config.urlDataKey);
+            // url has been/is being shortened
+            if (btz[url]) {
+                // has been shortened: use it
+                if (btz[url].short)
+                    $link
+                        // mark to avoid re-bitlyze-ing
+                        .addClass('bitlyzed')
+                        // replace url with short
+                        .data('url', btz[url].short);
+                // is being shortened: queue for it
+                else btz[url].queue.push(this);
+                return;
+            }
+            // create url map && queue current element
+            btz[url] = { queue: [this] };
+            var bitlyUrl = config.bitlyUrl;
+            // use either ? or &
+            bitlyUrl += bitlyUrl.indexOf('?') === -1 ? '?' : '&';
+            // add format
+            bitlyUrl += 'format=txt';
+            // add acdess token
+            bitlyUrl += '&access_token=' + config.accessToken;
+            // add long url
+            bitlyUrl += '&longUrl=' + encodeURIComponent(url);
+            $.get(bitlyUrl, function (short) {
+                // add short url to map
+                btz[url].short = short;
+                // settle queue
+                for (var i = 0; i < btz[url].queue.length; i++) {
+                    $(btz[url].queue[i])
+                        // mark to avoid re-bitlyze-ing
+                        .addClass('bitlyzed')
+                        // replace url with short
+                        .data(config.urlDataKey, short);
+                    // call callback on current queued element
+                    config.callback.call(btz[url].queue[i], short);
+                }
+                // empty queue
+                btz[url].queue = [];
+                // save
+                localStorage.setItem('btz', JSON.stringify(btz));
+            });
+        });
+        return this;
+    };
+})(jQuery);
